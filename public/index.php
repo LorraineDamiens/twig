3b30515474f3e34fe672502b1830bc607294b785
D <lorrainedams@me.com>
2019-11-16 10:56:25 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$twig->render('index.html.twig', $products(
+    'product1'=> 'banane',
+    'product2'=> 'fraise',
+    'product3'=> 'citron',
+    'product4'=> 'kiwi',
+    'product5'=> 'pomme'));
